162411610353c7aa4de95060a04e1a3dd5427519
Vibha Hegde <160986860+Vibha-Hegde2003@users.noreply.github.com>
2025-07-28 11:30:09 +0530

    Update invoke.java

--- src/test/java/main/invoke.java
+++ src/test/java/main/invoke.java
@@ -1,5 +1,27 @@
-package main;
+import java.util.List;
+import org.testng.TestNG;
+import org.testng.collections.Lists;
 
-public class invoke {
-
+public class invoke {   // DO NOT change the class name
+	
+	public void checkAgent() {            // DO NOT change the method signature
+	    //Invoke the test using TestNG ONLY HERE.
+    	try {
+    		TestNG testng = new TestNG();
+    		List<String> suites = Lists.newArrayList();
+    		String workingDir = System.getProperty("user.dir");
+    		System.out.println("user.dir "+ workingDir);
+    		suites.add(workingDir + "/testng.xml");
+    		
+    		testng.setTestSuites(suites);
+    		testng.run();
+		} catch (Exception e) {	 	  	  		 	     	     	      	 	
+			//e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+	    invoke iv=new invoke();
+	    iv.checkAgent();
+	}
 }
