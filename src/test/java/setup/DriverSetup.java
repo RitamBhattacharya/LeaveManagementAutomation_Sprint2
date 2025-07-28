e154128257f9a6a2ec942f6a24ca993b48f9e679
Vibha Hegde <160986860+Vibha-Hegde2003@users.noreply.github.com>
2025-07-28 12:42:16 +0530

    Update DriverSetup.java

--- src/test/java/setup/DriverSetup.java
+++ src/test/java/setup/DriverSetup.java
@@ -1,5 +1,32 @@
 package setup;
 
-public class DriverSetup {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
+public class DriverSetup {
+    public static WebDriver driver;
+    public static ConfigFileReader filereader = new ConfigFileReader();
+
+    public static WebDriver getDriver() {   // DO NOT CHANGE OR MODIFY THE METHOD SIGNATURE
+
+        if(driver == null) {
+            //Get the drivername and driverpath by reading the configuration file.
+
+            System.setProperty(filereader.getDriverName(), filereader.getDriverPath());
+
+            FirefoxBinary firefoxBinary = new FirefoxBinary();
+//            firefoxBinary.addCommandLineOptions("--headless");
+            FirefoxProfile profile=new FirefoxProfile();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setBinary(firefoxBinary);
+            firefoxOptions.setProfile(profile);
+
+            driver = new FirefoxDriver(firefoxOptions);
+        }
+
+        return driver;
+    }
 }
