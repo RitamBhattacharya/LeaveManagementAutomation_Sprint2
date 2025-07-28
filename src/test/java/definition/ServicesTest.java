1af8b0092f3b49a901b1b20f83b66fb4e51895c6
anamikas17 <143887883+anamikas17@users.noreply.github.com>
2025-07-28 10:36:55 +0530

    Update ServicesTest.java

--- src/test/java/definition/ServicesTest.java
+++ src/test/java/definition/ServicesTest.java
@@ -1,5 +1,16 @@
 package definition;
 
-public class ServicesTest {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(
+    features = "src/test/resources/features",        // path to your feature files
+    glue = {"steps"},                                // package with step definitions
+    plugin = {"pretty", "html:target/cucumber-reports.html"},
+    monochrome = true,
+    tags = "@SmokeTest or @RegressionTest"           // optional, filter by tag
+)
+public class ServicesTest extends AbstractTestNGCucumberTests {
+    // This class will automatically pick all feature files and run corresponding step definitions
 }
+
